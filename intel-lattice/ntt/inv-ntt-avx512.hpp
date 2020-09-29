bfa19c383378f98f3e80cd682f7c45755ebf8b23
Fabian Boemer <fabian.boemer@intel.com>
2020-09-29 14:20:35 -0700

    Extracted InvT2 => 75.9us

--- intel-lattice/ntt/inv-ntt-avx512.hpp
+++ intel-lattice/ntt/inv-ntt-avx512.hpp
@@ -119,6 +119,63 @@ void InvT1(uint64_t* elements, __m512i v_modulus, __m512i v_twice_mod,
 }
 
 template <int BitShift>
+void InvT2(uint64_t* elements, __m512i v_modulus, __m512i v_twice_mod,
+           uint64_t m, const uint64_t* W_op, const uint64_t* W_precon) {
+  size_t j1 = 0;
+
+  // 4 | m guaranteed by n >= 16
+  for (size_t i = m / 4; i > 0; --i) {
+    uint64_t* X = elements + j1;
+    uint64_t* Y = X + 2;
+
+    __m512i v_X =
+        _mm512_set_epi64(X[13], X[12], X[9], X[8], X[5], X[4], X[1], X[0]);
+    __m512i v_Y =
+        _mm512_set_epi64(Y[13], Y[12], Y[9], Y[8], Y[5], Y[4], Y[1], Y[0]);
+
+    __m512i v_W_op = _mm512_set_epi64(W_op[3], W_op[3], W_op[2], W_op[2],
+                                      W_op[1], W_op[1], W_op[0], W_op[0]);
+    __m512i v_W_precon =
+        _mm512_set_epi64(W_precon[3], W_precon[3], W_precon[2], W_precon[2],
+                         W_precon[1], W_precon[1], W_precon[0], W_precon[0]);
+
+    __m512i v_tx = _mm512_add_epi64(v_X, v_Y);
+    __m512i tmp_ty = _mm512_add_epi64(v_X, v_twice_mod);
+    __m512i v_ty = _mm512_sub_epi64(tmp_ty, v_Y);
+    v_X = _mm512_il_small_mod_epi64(v_tx, v_twice_mod);
+    __m512i v_Q = _mm512_il_mulhi_epi<BitShift>(v_W_precon, v_ty);
+    __m512i tmp_y1 = _mm512_mullo_epi64(v_ty, v_W_op);
+    __m512i tmp_y2 = _mm512_mullo_epi64(v_Q, v_modulus);
+    v_Y = _mm512_sub_epi64(tmp_y1, tmp_y2);
+
+    uint64_t* X_out = reinterpret_cast<uint64_t*>(&v_X);
+    uint64_t* Y_out = reinterpret_cast<uint64_t*>(&v_Y);
+
+    *X++ = X_out[0];
+    *X++ = X_out[1];
+    *X++ = Y_out[0];
+    *X++ = Y_out[1];
+    *X++ = X_out[2];
+    *X++ = X_out[3];
+    *X++ = Y_out[2];
+    *X++ = Y_out[3];
+    *X++ = X_out[4];
+    *X++ = X_out[5];
+    *X++ = Y_out[4];
+    *X++ = Y_out[5];
+    *X++ = X_out[6];
+    *X++ = X_out[7];
+    *X++ = Y_out[6];
+    *X++ = Y_out[7];
+
+    W_op += 4;
+    W_precon += 4;
+
+    j1 += 16;
+  }
+}
+
+template <int BitShift>
 void InverseTransformFromBitReverseAVX512(
     const uint64_t n, const uint64_t mod,
     const uint64_t* inv_root_of_unity_powers,
@@ -159,6 +216,15 @@ void InverseTransformFromBitReverseAVX512(
       continue;
     }
 
+    if (t == 2) {
+      const uint64_t* W_op = &inv_root_of_unity_powers[root_index];
+      const uint64_t* W_precon = &precon_inv_root_of_unity_powers[root_index];
+      InvT2<BitShift>(elements, v_modulus, v_twice_mod, m, W_op, W_precon);
+      t <<= 1;
+      root_index += m;
+      continue;
+    }
+
     for (size_t i = 0; i < m; i++, root_index++) {
       const uint64_t W_op = inv_root_of_unity_powers[root_index];
       const uint64_t W_precon = precon_inv_root_of_unity_powers[root_index];
@@ -166,8 +232,6 @@ void InverseTransformFromBitReverseAVX512(
       uint64_t* X = elements + j1;
       uint64_t* Y = X + t;
 
-      uint64_t tx;
-      uint64_t ty;
       if (t == 4) {
         __m256i v_W_op = _mm256_set1_epi64x(W_op);
         __m256i v_W_precon = _mm256_set1_epi64x(W_precon);
@@ -199,19 +263,6 @@ void InverseTransformFromBitReverseAVX512(
 
         _mm256_storeu_si256(v_X_pt, v_X);
         _mm256_storeu_si256(v_Y_pt, v_Y);
-
-      } else if (t == 2) {
-        tx = *X + *Y;
-        ty = *X + twice_mod - *Y;
-        *X++ = tx - (twice_mod & static_cast<uint64_t>(
-                                     (-static_cast<int64_t>(tx >= twice_mod))));
-        *Y++ = MultiplyUIntModLazy<BitShift>(ty, W_op, W_precon, mod);
-
-        tx = *X + *Y;
-        ty = *X + twice_mod - *Y;
-        *X = tx - (twice_mod & static_cast<uint64_t>(
-                                   (-static_cast<int64_t>(tx >= twice_mod))));
-        *Y = MultiplyUIntModLazy<BitShift>(ty, W_op, W_precon, mod);
       }
       j1 += (t << 1);
     }
