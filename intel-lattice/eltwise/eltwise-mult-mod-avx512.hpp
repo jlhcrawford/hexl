d8617594af70f237e06e1403b7f3af406d07200c
gseifu <gelila.seifu@intel.com>
2020-12-10 14:51:45 -0800

    missing declaration

--- intel-lattice/eltwise/eltwise-mult-mod-avx512.hpp
+++ intel-lattice/eltwise/eltwise-mult-mod-avx512.hpp
@@ -25,7 +25,6 @@ namespace lattice {
 void EltwiseMultModAVX512Int(uint64_t* operand1, const uint64_t* operand2,
                              uint64_t n, const uint64_t modulus);
 
-// Algorithm 1 from https://hal.archives-ouvertes.fr/hal-01215845/document
 void EltwiseMultModAVX512IntOofP(uint64_t* result, uint64_t* operand1,
                                  const uint64_t* operand2, uint64_t n,
                                  const uint64_t modulus);
@@ -35,6 +34,10 @@ void EltwiseMultModAVX512IntOofP(uint64_t* result, uint64_t* operand1,
 // https://hal.archives-ouvertes.fr/hal-02552673/document
 void EltwiseMultModAVX512Float(uint64_t* operand1, const uint64_t* operand2,
                                uint64_t n, const uint64_t modulus);
+
+void EltwiseMultModAVX512FloatOofP(uint64_t* result, uint64_t* operand1,
+                                   const uint64_t* operand2, uint64_t n,
+                                   const uint64_t modulus);
 
 }  // namespace lattice
 }  // namespace intel
